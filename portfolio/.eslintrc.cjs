03309375c143244b9196288cdc1644a937c8ae2a
Geryenko Hawsen <gery.zarons@gmail.com>
2024-01-22 22:03:45 +0900

    add prettier

--- portfolio/.eslintrc.cjs
+++ portfolio/.eslintrc.cjs
@@ -1,23 +1,23 @@
 module.exports = {
   root: true,
-  extends: ['@nuxt/eslint-config'],
+  extends: ["@nuxt/eslint-config", "prettier"],
   rules: {
     // disable the rule for all files
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    "@typescript-eslint/explicit-function-return-type": "off",
   },
   overrides: [
     {
       // enable the rule specifically for TypeScript files
-      files: ['*.ts', '*.mts', '*.cts', '*.tsx', '*.vue'],
+      files: ["*.ts", "*.mts", "*.cts", "*.tsx", "*.vue"],
       rules: {
-        '@typescript-eslint/explicit-function-return-type': 'warn',
-        'no-unused-vars': 'off',
-        '@typescript-eslint/no-unused-vars': [
-          'warn', // or "error"
+        "@typescript-eslint/explicit-function-return-type": "warn",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": [
+          "warn", // or "error"
           {
-            argsIgnorePattern: '^_',
-            varsIgnorePattern: '^_',
-            caughtErrorsIgnorePattern: '^_',
+            argsIgnorePattern: "^_",
+            varsIgnorePattern: "^_",
+            caughtErrorsIgnorePattern: "^_",
           },
         ],
       },
---
+++ portfolio/.prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 120,
+  arrowParens: "avoid",
+  endOfLine: "lf",
+};
