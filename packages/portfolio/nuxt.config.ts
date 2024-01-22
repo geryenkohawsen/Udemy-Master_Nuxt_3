f60c6a5acffae2aed3de93a26726aea2c2fdc590
Geryenko Hawsen <gery.zarons@gmail.com>
2024-01-23 00:12:45 +0900

    yarn lintfix

--- packages/portfolio/nuxt.config.ts
+++ packages/portfolio/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/eslint-module", "@nuxtjs/tailwindcss"],
-  components: [{ path: "~/components", pathPrefix: false }],
-});
+  modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss'],
+  components: [{ path: '~/components', pathPrefix: false }],
+})
