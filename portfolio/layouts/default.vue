7f0963eeea206d8a5fb54af61b20d71e109d6738
Geryenko Hawsen <gery.zarons@gmail.com>
2024-01-22 23:29:18 +0900

    17. Styling the App with Tailwind CSS

--- portfolio/layouts/default.vue
+++ portfolio/layouts/default.vue
@@ -1,15 +1,24 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <NuxtLink to="/"> Main </NuxtLink>
-        </li>
-        <li>
-          <NuxtLink to="/about"> About </NuxtLink>
-        </li>
-      </ul>
-    </nav>
-    <slot />
+  <div class="container mx-auto max-w-2xl">
+    <header class="mt-5 flex items-center justify-between">
+      <div>
+        <NuxtLink to="/" class="p-2 text-xl font-semibold hover:bg-gray-200">Gery</NuxtLink>
+      </div>
+
+      <nav class="font-mono">
+        <ul class="flex space-x-4">
+          <li>
+            <NuxtLink to="/">Main</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <main class="p-2">
+      <slot />
+    </main>
   </div>
 </template>
