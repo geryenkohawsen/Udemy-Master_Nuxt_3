06f43dfa80ca6285a4599e1b5beb938fcdbc9819
Geryenko Hawsen <gery.zarons@gmail.com>
2024-01-22 21:48:22 +0900

    add eslint rules

--- portfolio/.eslintrc.cjs
+++ portfolio/.eslintrc.cjs
@@ -1,4 +1,26 @@
 module.exports = {
   root: true,
   extends: ['@nuxt/eslint-config'],
+  rules: {
+    // disable the rule for all files
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.mts', '*.cts', '*.tsx', '*.vue'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'warn',
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'warn', // or "error"
+          {
+            argsIgnorePattern: '^_',
+            varsIgnorePattern: '^_',
+            caughtErrorsIgnorePattern: '^_',
+          },
+        ],
+      },
+    },
+  ],
 };
