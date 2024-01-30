d1a03b5b19ff529e30ef18e22e58e1ef03b82141
Geryenko Hawsen <gery.zarons@gmail.com>
2024-01-30 22:12:17 +0900

    update nuxt config

--- packages/portfolio/nuxt.config.ts
+++ packages/portfolio/nuxt.config.ts
@@ -1,10 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   components: [{ path: '~/components', pathPrefix: false }],
   imports: {
     dirs: ['types/*.ts'],
+  },
+  eslint: {
+    lintOnStart: false,
   },
   colorMode: {
     classSuffix: '',
