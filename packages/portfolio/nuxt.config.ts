d15e33a99baf4ee19f928499540b662d587a120d
Geryenko Hawsen <gery.zarons@gmail.com>
2024-02-01 21:20:01 +0900

    29. Images and Code Highlighting

--- packages/portfolio/nuxt.config.ts
+++ packages/portfolio/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   devtools: { enabled: true },
   modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxt/content'],
   components: [{ path: '~/components', pathPrefix: false }],
@@ -9,6 +9,11 @@ export default defineNuxtConfig({
   },
   eslint: {
     lintOnStart: false,
+  },
+  content: {
+    highlight: {
+      theme: 'monokai',
+    },
   },
   colorMode: {
     classSuffix: '',
--- packages/portfolio/pages/blog/[...slug].vue
+++ packages/portfolio/pages/blog/[...slug].vue
@@ -4,7 +4,9 @@ console.log('route --> ', route.params.slug)
 </script>
 
 <template>
-  <article class="prose dark:prose-invert">
+  <article
+    class="prose dark:prose-invert prose-pre:bg-white prose-pre:text-gray-700 dark:prose-pre:bg-gray-800 dark:prose-pre:text-gray-300"
+  >
     <ContentDoc />
   </article>
 </template>
