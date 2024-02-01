0296ff6338d2c8a1a375f77e532ac95b1330bfde
Geryenko Hawsen <gery.zarons@gmail.com>
2024-02-02 01:59:31 +0900

    35. Table of Contents: Slots

--- packages/portfolio/pages/blog/[...slug].vue
+++ packages/portfolio/pages/blog/[...slug].vue
@@ -5,8 +5,11 @@ console.log('route --> ', route.params.slug)
 
 <template>
   <article
-    class="prose dark:prose-invert prose-pre:bg-white prose-pre:text-gray-700 dark:prose-pre:bg-gray-800 dark:prose-pre:text-gray-300"
+    class="prose max-w-none dark:prose-invert prose-pre:bg-white prose-pre:text-gray-700 dark:prose-pre:bg-gray-800 dark:prose-pre:text-gray-300"
   >
-    <ContentDoc />
+    <ContentDoc v-slot="{ doc }">
+      {{ doc.body?.toc?.links }}
+      <ContentRenderer :value="doc" />
+    </ContentDoc>
   </article>
 </template>
