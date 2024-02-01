d386ac79b3aee3b80f97c5f370ab8e1629826b79
Geryenko Hawsen <gery.zarons@gmail.com>
2024-02-01 21:37:16 +0900

    30. Blog Post List - Querying Content

--- packages/portfolio/pages/blog/index.vue
+++ packages/portfolio/pages/blog/index.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+const { data: posts } = await useAsyncData('blog-list', () => queryContent('/blog').only(['_path', 'title']).find())
+console.log('posts --> ', posts)
+</script>
+
 <template>
-  <div>All blog</div>
+  <section>
+    <ul>
+      <li v-for="post in posts" :key="post._path">
+        <NuxtLink :to="post._path">
+          {{ post.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+  </section>
 </template>
