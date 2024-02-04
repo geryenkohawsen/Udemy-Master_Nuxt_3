6e633b3db3773788130daca5029639b6b19d8f0e
Geryenko Hawsen <gery.zarons@gmail.com>
2024-02-04 16:17:51 +0900

    add return back function to error page

--- packages/portfolio/error.vue
+++ packages/portfolio/error.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
 import type { NuxtError } from '#app'
 
-defineProps({
-  error: {
-    type: Object as () => NuxtError,
-    default: () => ({}),
-  },
-})
+interface Props {
+  error: NuxtError
+}
 
-const handleError = () => clearError({ redirect: '/' })
+defineProps<Props>()
+
+const handleError = (): Promise<void> => clearError({ redirect: '/' })
 </script>
 
 <template>
-  <NuxtLayout>
-    <section>
-      <h1 class="mb-4 text-4xl">Opps {{ error.statusCode }}</h1>
-      <h2 class="text-xl text-gray-500 dark:text-gray-400">{{ error.statusMessage }}</h2>
-    </section>
-  </NuxtLayout>
+  <section class="flex flex-col">
+    <h1 class="mb-4 text-4xl">Opps {{ error.statusCode }}</h1>
+    <h2 class="text-xl text-gray-500 dark:text-gray-400">{{ error.statusMessage }}</h2>
+    <button
+      class="rounded bg-emerald-600 px-4 py-2 font-bold uppercase text-white hover:bg-emerald-700 dark:bg-teal-700 dark:hover:bg-teal-800"
+      @click="handleError"
+    >
+      go to home page
+    </button>
+  </section>
 </template>
