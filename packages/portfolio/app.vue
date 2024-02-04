3c179278ca7a9a6444dd5140e42c453cb722b657
Geryenko Hawsen <gery.zarons@gmail.com>
2024-02-04 16:39:06 +0900

    40. Page Transitions

--- packages/portfolio/app.vue
+++ packages/portfolio/app.vue
@@ -8,3 +8,16 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.3s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0.3;
+  filter: blur(1rem);
+  transform: rotate3d(1, 1, 1, 15deg);
+}
+</style>
--- packages/portfolio/nuxt.config.ts
+++ packages/portfolio/nuxt.config.ts
@@ -4,6 +4,9 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/eslint-module', '@nuxtjs/tailwindcss', '@nuxtjs/color-mode', '@nuxt/content'],
   components: [{ path: '~/components', pathPrefix: false }],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }, // outgoing page will be animated first before the ingoing page
+  },
   imports: {
     dirs: ['types/*.ts'],
   },
