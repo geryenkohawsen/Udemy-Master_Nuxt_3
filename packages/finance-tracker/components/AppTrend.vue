0a7b989377ada601e476cd26cb907186cf41fd6e
Geryenko Hawsen <gery.zarons@gmail.com>
2024-02-12 18:06:53 +0900

    57. Trend Component (Icons, Spinners)

---
+++ packages/finance-tracker/components/AppTrend.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface Props {
+  title: string
+  amount: number
+  lastAmount: number
+  color: 'green' | 'red'
+  isLoading: boolean
+}
+
+const props = defineProps<Props>()
+</script>
+
+<template>
+  <div>
+    <div class="font-bold" :class="[props.color]">{{ props.title }}</div>
+    <div class="mb-2 text-2xl font-extrabold text-black dark:text-white">
+      <USkeleton v-if="props.isLoading" class="h-8 w-full" />
+      <div v-else>{{ props.amount }}</div>
+    </div>
+    <div>
+      <USkeleton v-if="props.isLoading" class="h-6 w-full" />
+      <div v-else class="flex items-center space-x-1 text-sm">
+        <UIcon name="i-heroicons-arrow-trending-up" class="h-6 w-6" :class="[props.color]" />
+        <div class="text-gray-500 dark:text-gray-400">30% vs last period</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.green {
+  @apply text-green-600 dark:text-green-400;
+}
+.red {
+  @apply text-red-600 dark:text-red-400;
+}
+</style>
--- packages/finance-tracker/pages/index.vue
+++ packages/finance-tracker/pages/index.vue
@@ -3,10 +3,19 @@ const selectedTransactionView = ref(transactionViewOptions[1])
 </script>
 
 <template>
-  <section class="mb-10 flex items-center justify-between">
-    <h1 class="text-4xl font-extrabold">Summary</h1>
-    <div>
-      <USelectMenu v-model="selectedTransactionView" :options="transactionViewOptions" />
-    </div>
-  </section>
+  <div id="page-index">
+    <section class="mb-10 flex items-center justify-between">
+      <h1 class="text-4xl font-extrabold">Summary</h1>
+      <div>
+        <USelectMenu v-model="selectedTransactionView" :options="transactionViewOptions" />
+      </div>
+    </section>
+
+    <section class="mb-10 grid grid-cols-1 sm:grid-cols-2 sm:gap-16 lg:grid-cols-4">
+      <AppTrend color="green" title="Income" :amount="4000" :last-amount="3000" :is-loading="false" />
+      <AppTrend color="red" title="Income" :amount="4000" :last-amount="3000" :is-loading="false" />
+      <AppTrend color="green" title="Income" :amount="4000" :last-amount="3000" :is-loading="false" />
+      <AppTrend color="red" title="Income" :amount="4000" :last-amount="3000" :is-loading="true" />
+    </section>
+  </div>
 </template>
