47c9deb60815057fb8b527a00a6d173e0d0be840
Geryenko Hawsen <gery.zarons@gmail.com>
2024-02-12 17:26:38 +0900

    56. Summary Section and Period Selection

--- packages/finance-tracker/pages/index.vue
+++ packages/finance-tracker/pages/index.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+const selectedTransactionView = ref(transactionViewOptions[1])
+</script>
+
 <template>
-  <div>Index page!</div>
+  <section class="mb-10 flex items-center justify-between">
+    <h1 class="text-4xl font-extrabold">Summary</h1>
+    <div>
+      <USelectMenu v-model="selectedTransactionView" :options="transactionViewOptions" />
+    </div>
+  </section>
 </template>
