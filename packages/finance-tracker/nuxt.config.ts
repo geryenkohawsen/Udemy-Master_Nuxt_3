9d50b116878c573df239c01f08c474b61e270989
Geryenko Hawsen <gery.zarons@gmail.com>
2024-02-18 00:24:30 +0900

    62. Using Supabase in Nuxt

--- packages/finance-tracker/nuxt.config.ts
+++ packages/finance-tracker/nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/eslint-module', '@nuxt/ui'],
+  modules: ['@nuxtjs/eslint-module', '@nuxt/ui', '@nuxtjs/supabase'],
   components: [{ path: '~/components', pathPrefix: false }],
   eslint: {
     lintOnStart: false,
   },
-});
+  supabase: {
+    redirect: false, // false for development
+  },
+})
