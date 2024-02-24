1557a72774a06a04f93775a3dbb66307c312e077
Geryenko Hawsen <gery.zarons@gmail.com>
2024-02-24 19:44:35 +0900

    add error handling to main page

--- packages/finance-tracker/pages/index.vue
+++ packages/finance-tracker/pages/index.vue
@@ -76,8 +76,10 @@ console.log('transactionsGroupedByDate → ', transactionsGroupedByDate.value)
     </section>
 
     <section class="mb-10 grid grid-cols-1 sm:grid-cols-2 sm:gap-16 lg:grid-cols-4">
-      <AppTrend title="Income" :amount="incomeTotal" :last-amount="3000" :is-loading="isLoading" />
-      <AppTrend title="Expense" :amount="expenseTotal" :last-amount="5000" :is-loading="isLoading" />
+      <AppTrend v-if="incomeTotal" title="Income" :amount="incomeTotal" :last-amount="3000" :is-loading="isLoading" />
+      <USkeleton v-else class="h-full w-full" />
+      <AppTrend v-if="expenseTotal" title="Expense" :amount="expenseTotal" :last-amount="5000" :is-loading="isLoading" />
+      <USkeleton v-else class="h-full w-full" />
       <AppTrend title="Investments" :amount="4000" :last-amount="3000" :is-loading="isLoading" />
       <AppTrend title="Saving" :amount="4000" :last-amount="4200" :is-loading="isLoading" />
     </section>
@@ -85,12 +87,11 @@ console.log('transactionsGroupedByDate → ', transactionsGroupedByDate.value)
     <section class="mb-10 flex justify-between">
       <div>
         <h2 class="text-2xl font-extrabold">Transactions</h2>
-        <div class="text-gray-500 dark:text-gray-400">
-          You have {{ incomeCount }} incomes and {{ expenseCount }} expense this period
-        </div>
+        <div class="text-gray-500 dark:text-gray-400">You have {{ incomeCount }} incomes and {{ expenseCount }} expense this period</div>
       </div>
-
-      <div><UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" /></div>
+      <div>
+        <UButton icon="i-heroicons-plus-circle" color="white" variant="solid" label="Add" />
+      </div>
     </section>
 
     <section v-if="isLoading">
@@ -99,12 +100,7 @@ console.log('transactionsGroupedByDate → ', transactionsGroupedByDate.value)
     <section v-else>
       <div v-for="(transactionsByDate, date) in transactionsGroupedByDate" :key="date">
         <AppDailyTransactionSummary :date="date.toString()" :transactions="transactionsByDate" />
-        <AppTransaction
-          v-for="transaction in transactionsByDate"
-          :key="transaction.id"
-          :transaction="transaction"
-          @deleted="refreshTransactions"
-        />
+        <AppTransaction v-for="transaction in transactionsByDate" :key="transaction.id" :transaction="transaction" @deleted="refreshTransactions" />
       </div>
     </section>
   </div>
