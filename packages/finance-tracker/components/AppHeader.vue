ae507272beec182dac4f1f8ef55c415acc6117ee
Geryenko Hawsen <gery.zarons@gmail.com>
2024-03-31 01:51:21 +0900

    96. User Profile: Nested Routes, Vertical Navigation and Settings Page

--- packages/finance-tracker/components/AppHeader.vue
+++ packages/finance-tracker/components/AppHeader.vue
@@ -12,7 +12,7 @@ const items = [
     {
       label: 'Settings',
       icon: 'i-heroicons-cog-8-tooth',
-      click: () => console.log('Link to settings in the future'),
+      click: () => navigateTo('/settings'),
     },
     {
       label: 'Sign out',
@@ -33,17 +33,17 @@ const items = [
     <UDropdown v-if="user" :items="items" :ui="{ item: { disabled: 'cursor-text select-text' }, width: 'w-64' }">
       <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
 
-      <template #account="{ item }">
+      <template #account>
         <div class="text-left">
           <p>Signed in as</p>
           <p class="font-medium text-gray-900 dark:text-white">
-            {{ item.label }}
+            {{ user.email }}
           </p>
         </div>
       </template>
 
       <template #item="{ item }">
-        <span class="truncate">{{ user.email }}</span>
+        <span class="truncate">{{ item.label }}</span>
 
         <UIcon :name="item.icon" class="ms-auto h-4 w-4 flex-shrink-0 text-gray-400 dark:text-gray-500" />
       </template>
---
+++ packages/finance-tracker/pages/settings.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const val = ref(0)
+
+const links = [
+  {
+    label: 'Profile',
+    to: '/settings/profile',
+  },
+  {
+    label: 'Avatar',
+    to: '/settings/avatar',
+  },
+  {
+    label: 'Settings',
+    to: '/settings/settings',
+  },
+]
+
+onMounted(() => {
+  setInterval(() => (val.value += 1), 1000)
+})
+</script>
+
+<template>
+  <div class="grid grid-cols-6 gap-8">
+    <UVerticalNavigation :links="links" class="col-span-2" />
+    <div class="col-span-4">
+      <div>{{ val }}</div>
+      <NuxtPage />
+    </div>
+  </div>
+</template>
