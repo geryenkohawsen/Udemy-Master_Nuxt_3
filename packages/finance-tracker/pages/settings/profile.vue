84aea2b159dabf15cc9edaa5b5ba1d11feb3cc05
Geryenko Hawsen <gery.zarons@gmail.com>
2024-03-31 17:50:36 +0900

    98. User Profile: Saving User Metadata & Changing Email

--- packages/finance-tracker/pages/settings/profile.vue
+++ packages/finance-tracker/pages/settings/profile.vue
@@ -8,7 +8,7 @@ const { toastSuccess, toastError } = useAppToast()
 const pending = ref(false)
 
 const state = ref({
-  name: '',
+  name: user.value?.user_metadata?.['full_name'],
   email: user.value?.email,
 })
 
@@ -16,10 +16,51 @@ const schema = z.object({
   name: z.string().min(2).optional(),
   email: z.string().email(),
 })
+
+async function saveProfile() {
+  pending.value = true
+
+  // Only add `full_name` and `email` if it's different from the current profile in the database
+  const data = {
+    ...(state.value.name !== user.value?.user_metadata?.['full_name'] && { full_name: state.value.name }),
+    ...(state.value.email !== user.value?.email && { email: state.value.email }),
+  }
+
+  // Return immediately if there are no changes
+  if (Object.keys(data).length === 0) {
+    toastSuccess({
+      title: 'Profile updated??',
+      description: 'There were no changes though 🤔',
+    })
+    pending.value = false
+    return
+  }
+
+  // If there are changes to the current profile data
+  const { error } = await supabase.auth.updateUser({
+    data: {
+      full_name: state.value.name,
+    },
+  })
+
+  if (error) {
+    toastError({
+      title: 'Error updating profile',
+      description: error.message,
+    })
+  } else {
+    toastSuccess({
+      title: 'Profile updated',
+      description: 'Your profile has been updated!',
+    })
+  }
+
+  pending.value = false
+}
 </script>
 
 <template>
-  <UForm :state="state" :schema="schema">
+  <UForm :state="state" :schema="schema" @submit="saveProfile">
     <UFormGroup class="mb-4" label="Full Name" name="name">
       <UInput v-model="state.name" />
     </UFormGroup>
@@ -33,6 +74,6 @@ const schema = z.object({
       <UInput v-model="state.email" />
     </UFormGroup>
 
-    <UButton type="submit" color="black" variant="solid" label="Save" :pending="pending" />
+    <UButton type="submit" color="black" variant="solid" label="Save" :pending="pending" :disabled="pending" />
   </UForm>
 </template>
