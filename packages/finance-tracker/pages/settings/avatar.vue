234d1f79dc7fbd3d012f3026fd427f76c0f091d6
Geryenko Hawsen <gery.zarons@gmail.com>
2024-04-01 01:57:38 +0900

    101. Uploading Files (Avatar Image): Getting the File

--- packages/finance-tracker/pages/settings/avatar.vue
+++ packages/finance-tracker/pages/settings/avatar.vue
@@ -1,3 +1,66 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient<Database>()
+const user = useSupabaseUser()
+
+// We need to get the actual avatar URL
+const { toastSuccess, toastError } = useAppToast()
+
+const uploading = ref(false)
+const fileInput = ref() // Reference to an input with ref="fileInput" attribute
+
+async function saveAvatar() {
+  // 1. Get the uploaded file
+  //    a) If no file uploaded, show toast error
+  // 2. Generate the new filename
+
+  const file = fileInput.value.input.files[0]
+
+  if (!file) {
+    toastError({
+      title: 'Select a file to upload first',
+    })
+    return
+  }
+
+  const fileExtension = file.name.split('.').pop()
+  const newFileName = `${Math.random()}.${fileExtension}`
+
+  // try {
+  //   uploading.value = true
+  //   // 1. Grab the current avatar URL
+  //   // 2. Upload the image to avatars bucket
+  //   // 3. Update the user metadata with the avatar URL
+  //   // 4. (OPTIONALLY) remove the old avatar file
+  //   // 5. Reset the file input
+
+  //   toastSuccess({
+  //     title: 'Avatar uploaded',
+  //   })
+  // } catch (error) {
+  //   toastError({
+  //     title: 'Error uploading avatar',
+  //     description: error.message,
+  //   })
+  // } finally {
+  //   uploading.value = false
+  // }
+}
+</script>
+
 <template>
-  <div>Avatar!</div>
+  <div>
+    <div class="mb-4">
+      <UFormGroup label="Current avatar" class="w-full" help="This would be blank by default">
+        <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" size="3xl" />
+      </UFormGroup>
+    </div>
+
+    <div class="mb-4">
+      <UFormGroup label="New avatar" class="w-full" name="avatar" help="After choosing an image click Save to actually upload the new avatar">
+        <UInput ref="fileInput" type="file" />
+      </UFormGroup>
+    </div>
+
+    <UButton type="submit" color="black" variant="solid" label="Save" :loading="uploading" :disabled="uploading" @click="saveAvatar" />
+  </div>
 </template>
