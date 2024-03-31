7d3bdb82d8d03e58bb4078ebfe8c31df4a7da248
Geryenko Hawsen <gery.zarons@gmail.com>
2024-04-01 02:52:02 +0900

    install dayjs module

--- packages/finance-tracker/nuxt.config.ts
+++ packages/finance-tracker/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/eslint-module', '@nuxt/ui', '@nuxtjs/supabase'],
+  modules: ['@nuxtjs/eslint-module', '@nuxt/ui', '@nuxtjs/supabase', 'dayjs-nuxt'],
   components: [{ path: '~/components', pathPrefix: false }],
   imports: {
     dirs: ['types/**'],
@@ -11,5 +11,11 @@ export default defineNuxtConfig({
   },
   supabase: {
     redirect: true, // false for development
+  },
+  dayjs: {
+    locales: ['ja'],
+    plugins: ['relativeTime', 'utc', 'timezone'],
+    defaultLocale: 'ja',
+    defaultTimezone: 'Asia/Japan',
   },
 })
