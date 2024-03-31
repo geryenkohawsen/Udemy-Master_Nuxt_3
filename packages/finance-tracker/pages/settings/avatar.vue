c9662f386f2d2e6972a7da4681431dd3359ae98f
Geryenko Hawsen <gery.zarons@gmail.com>
2024-04-01 02:54:46 +0900

    102. Uploading Files (Avatar Image): Actual Upload to Storage and Access Policies

--- packages/finance-tracker/pages/settings/avatar.vue
+++ packages/finance-tracker/pages/settings/avatar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 const supabase = useSupabaseClient<Database>()
 const user = useSupabaseUser()
+const dayjs = useDayjs()
 
 // We need to get the actual avatar URL
 const { toastSuccess, toastError } = useAppToast()
@@ -9,41 +10,57 @@ const uploading = ref(false)
 const fileInput = ref() // Reference to an input with ref="fileInput" attribute
 
 async function saveAvatar() {
+  if (!user.value) {
+    toastError({
+      title: 'Cannot retrieve user data',
+      description: 'Maybe try reloading the page?',
+    })
+    return
+  }
   // 1. Get the uploaded file
   //    a) If no file uploaded, show toast error
   // 2. Generate the new filename
+  const avatarFile = fileInput.value.input.files[0]
 
-  const file = fileInput.value.input.files[0]
-
-  if (!file) {
+  if (!avatarFile) {
     toastError({
       title: 'Select a file to upload first',
     })
     return
   }
 
-  const fileExtension = file.name.split('.').pop()
-  const newFileName = `${Math.random()}.${fileExtension}`
+  uploading.value = true
 
-  // try {
-  //   uploading.value = true
-  //   // 1. Grab the current avatar URL
-  //   // 2. Upload the image to avatars bucket
-  //   // 3. Update the user metadata with the avatar URL
-  //   // 4. (OPTIONALLY) remove the old avatar file
-  //   // 5. Reset the file input
+  const uploadTime = dayjs(new Date()).format('YYYYMMDDTHHmmss')
+  const fileExtension = avatarFile.name.split('.').pop()
+  const avatarFileName = `avatar-${user.value.email}-${uploadTime}.${fileExtension}`
 
-  //   toastSuccess({
-  //     title: 'Avatar uploaded',
-  //   })
-  // } catch (error) {
-  //   toastError({
-  //     title: 'Error uploading avatar',
-  //     description: error.message,
-  //   })
-  // } finally {
-  //   uploading.value = false
-  // }
+  // 1. Grab the current avatar URL
+  const currentAvatarUrl = user.value?.user_metadata?.['avatar_url']
+  // 2. Upload the image to avatars bucket
+  const { error } = await supabase.storage.from('avatars').upload(avatarFileName, avatarFile)
+
+  if (error) {
+    toastError({
+      title: 'Error updating avatar',
+      description: error.message,
+    })
+  }
+  // 3. Update the user metadata with the avatar URL
+  await supabase.auth.updateUser({
+    data: {
+      avatar_url: avatarFileName,
+    },
+  })
+  // 4. (OPTIONALLY) remove the old avatar file
+  // 5. Reset the file input
+  fileInput.value.input.value = null
+
+  toastSuccess({
+    title: 'Avatar updated',
+    description: 'Your avatar has been updated!',
+  })
+  uploading.value = false
 }
 </script>
 
