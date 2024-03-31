e219cfda73896807ba0559eda415102f36f11574
Geryenko Hawsen <gery.zarons@gmail.com>
2024-03-31 23:45:54 +0900

    99. User Profile: Storing and Using App User Preferences

--- packages/finance-tracker/pages/settings/settings.vue
+++ packages/finance-tracker/pages/settings/settings.vue
@@ -1,3 +1,65 @@
+<script setup lang="ts">
+import { z } from 'zod'
+
+const supabase = useSupabaseClient()
+const user = useSupabaseUser()
+const { toastSuccess, toastError } = useAppToast()
+const pending = ref(false)
+const state = ref({
+  transactionView: user.value?.user_metadata?.['transaction_view'], // Initialization goes here
+})
+
+const schema = z.object({
+  transactionView: z.nativeEnum(TransactionViewOptions), // ZOD constraints goes here
+})
+
+async function saveSettings() {
+  pending.value = true
+
+  // Only add `full_name` and `email` if it's different from the current profile in the database
+  const data = {
+    ...(state.value.transactionView !== user.value?.user_metadata?.['transaction_view'] && { transaction_view: state.value.transactionView }),
+  }
+
+  // Return immediately if there are no changes
+  if (Object.keys(data).length === 0) {
+    toastSuccess({
+      title: 'Settings updated??',
+      description: 'There were no changes though 🤔',
+    })
+    pending.value = false
+    return
+  }
+
+  // If there are changes to the current profile data
+  const { error } = await supabase.auth.updateUser({
+    data: {
+      transaction_view: state.value.transactionView,
+    },
+  })
+
+  if (error) {
+    toastError({
+      title: 'Error updating settings',
+      description: error.message,
+    })
+  } else {
+    toastSuccess({
+      title: 'Settings updated',
+      description: 'Your settings has been updated!',
+    })
+  }
+
+  pending.value = false
+}
+</script>
+
 <template>
-  <div>Settings!</div>
+  <UForm :state="state" :schema="schema" @submit="saveSettings">
+    <UFormGroup label="Transaction View" class="mb-4" help="Choose how you would like to view transactions">
+      <USelect v-model="state.transactionView" :options="CONST.TRANSACTION_VIEW_OPTIONS" />
+    </UFormGroup>
+
+    <UButton type="submit" color="black" variant="solid" label="Save" :loading="pending" :disabled="pending" />
+  </UForm>
 </template>
--- packages/finance-tracker/utils/constants.ts
+++ packages/finance-tracker/utils/constants.ts
@@ -1,9 +1,30 @@
-const transactionViewOptions = ['Yearly', 'Monthly', 'Daily']
-const expenseCategories = ['Food', 'Housing', 'Car', 'Entertainment', 'Transport']
-const transactionTypes = ['Income', 'Expense', 'Saving', 'Investment']
-
-export const CONST = {
-  EXPENSE_CATEGORIES: expenseCategories,
-  TRANSACTION_VIEW_OPTIONS: transactionViewOptions,
-  TRANSACTION_TYPES: transactionTypes,
+export enum TransactionViewOptions {
+  Yearly = 'Yearly',
+  Monthly = 'Monthly',
+  Daily = 'Daily',
 }
+
+export enum ExpenseCategories {
+  Food = 'Food',
+  Housing = 'Housing',
+  Car = 'Car',
+  Entertainment = 'Entertainment',
+  Transport = 'Transport',
+}
+
+export enum TransactionTypes {
+  Income = 'Income',
+  Expense = 'Expense',
+  Saving = 'Saving',
+  Investment = 'Investment',
+}
+
+// Convert enum values to arrays and assign to the CONST object
+export const CONST = {
+  EXPENSE_CATEGORIES: Object.values(ExpenseCategories),
+  TRANSACTION_VIEW_OPTIONS: Object.values(TransactionViewOptions),
+  TRANSACTION_TYPES: Object.values(TransactionTypes),
+}
+
+// Usage example
+// console.log(CONST.TRANSACTION_VIEW_OPTIONS) // ["Yearly", "Monthly", "Daily"]
